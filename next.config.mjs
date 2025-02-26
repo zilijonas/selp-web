6a5c2c15c4e9ede485d666ff72fa88f838ef9f4f
Linas Jonas <zilinskaslj@gmail.com>
2025-02-26 11:51:26 +0200

    fix gh pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/selp-web' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/selp-web/' : '',
   images: {
     unoptimized: true,
   },
